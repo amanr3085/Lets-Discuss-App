46d94c427e6c833937954f787d982e9a5ea45ed1
Aman Raj <amanr3085@gmail.com>
2022-01-10 00:41:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/message");
+const {
+  userJoin,
+  getCurentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "Let's Discuss Bot";
+
+//Run when a client connect
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    //welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to Let's Discuss!"));
+
+    //Broadcast when a user connect
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    //Send user and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  //Listen for chatMeassage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnect
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+      //Send user and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
